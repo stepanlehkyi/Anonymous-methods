082ceefa8a0c7c7466a3c156071a57315ea9aaf0
stepanlehkyi <60749566+stepanlehkyi@users.noreply.github.com>
2020-06-02 11:09:21 +0300

    Homework

---
+++ Anonymous Type/Anonymous Type/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*1. Create an anonymous method that takes a string as its argument and returns the string in reversed order.
+
+Examples
+
+Reverse("Hello World") ➞ "dlroW olleH"
+
+Reverse("The quick brown fox.") ➞ ".xof nworb kciuq ehT"
+
+Reverse("Edabit is really helpful!") ➞ "!lufpleh yllaer si tibadE“
+*/
+namespace Anonymous_Type
+{
+    public delegate string Reverse(string word);
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Reverse reverse = delegate (string word)
+            {
+                return new string(word.Reverse().ToArray());
+            };
+            string test1 =  reverse("Hello World");
+            string test2 = reverse("The quick brown fox.");
+            string test3 = reverse("Edabit is really helpful!");
+            Console.WriteLine(test1);
+            Console.WriteLine(test2);
+            Console.WriteLine(test3);
+        }
+    }
+}
---
+++ Anonymous Type/Task 1.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+2. Create a function an anonymous method that takes an array of numbers between 1 and 10 (excluding one number) and returns the
+missing number.
+
+Examples
+
+MissingNum([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
+
+MissingNum([7, 2, 3, 6, 5, 9, 1, 4, 8]) ➞ 10
+
+MissingNum([10, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 7
+*/
+namespace Task_1._2
+{
+    public delegate int MissingNum(int[] array);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MissingNum missingNum = delegate (int[] array)
+             {
+                 return Enumerable.Range(1, 10).Except(array).Last();
+             };
+            int missNumberOne = missingNum(new int[] { 7, 2, 3, 6, 5, 9, 1, 4, 8 });
+            int missNumberTwo = missingNum(new int[] { 1, 2, 3, 4, 6, 7, 8, 9, 10 });
+            int missNumberThree = missingNum(new int[] { 10, 5, 1, 2, 4, 6, 8, 3, 9 });
+            Console.WriteLine( missNumberOne + " "+ missNumberTwo + " " + missNumberThree);
+
+
+        }
+    }
+}
